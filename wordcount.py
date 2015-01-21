0f634b36a605aa9732611edf33db02b1780365f2
Hackbright Student <info@hackbrightacademy.com>
2015-01-21 12:13:26 -0800

    added function to make a dict of wordcounts per word in file

--- wordcount.py
+++ wordcount.py
@@ -4,13 +4,26 @@ script, textfile = argv
 
 file_object = open(textfile)
 
-counter = 0
-for each_line in file_object:
-    line = each_line.rstrip()
-    line_words = line.split(" ")
 
-    counter = counter + len(line_words)
-print counter
+
+def make_word_occurrence_dict(the_file):
+    
+    word_occurrence_dict = {}
+    
+    for each_line in file_object:
+        line = each_line.rstrip()
+        line = line.split(" ")
+
+        for each in line:
+            if each in word_occurrence_dict:
+                word_count = word_occurrence_dict.get(each)
+                word_occurrence_dict[each] = word_count + 1
+            else:
+                word_occurrence_dict[each] = 1
+
+    print word_occurrence_dict
+
+make_word_occurrence_dict(file_object)
 
 # TODO get a count for the number that each word appears in the file,
 # NOT a straight up word count.
