2f1ef49298d02420c340e641aa80a64b8e25a59e
Hackbright Student <info@hackbrightacademy.com>
2015-01-21 12:22:16 -0800

    Prettified the output

--- wordcount.py
+++ wordcount.py
@@ -21,9 +21,17 @@ def make_word_occurrence_dict(the_file):
             else:
                 word_occurrence_dict[each] = 1
 
-    print word_occurrence_dict
+    return word_occurrence_dict
 
-make_word_occurrence_dict(file_object)
+def print_word_counts(a_dictionary):
+    """This can take the output of word_occurrence_dict and prettify it 
+    to print out."""
+
+    for each_item in a_dictionary.items():
+        print 'the word %s appears in the file %s times.' % each_item
+
+words = make_word_occurrence_dict(file_object)
+print_word_counts(words)
 
 # TODO get a count for the number that each word appears in the file,
 # NOT a straight up word count.
